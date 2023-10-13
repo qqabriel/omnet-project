6f8f013e2b7ef2190baf208f83b9e7bcb9d1644c
qqabriel <131778920+qqabriel@users.noreply.github.com>
2023-10-13 14:32:23 +0200

    Add files via upload

---
+++ myWireless.cc
@@ -0,0 +1,12 @@
+void StandardHost::handleMessage(cMessage *msg)
+{
+    if (msg->isSelfMessage()) {
+        // Generate a new packet
+        cPacket *packet = new cPacket("DataPacket");
+        int packetBitLength = 2000; // Set the desired packet length in bits
+        packet->setBitLength(packetBitLength);
+
+        // Send the packet
+        send(packet, "out");
+    }
+}
